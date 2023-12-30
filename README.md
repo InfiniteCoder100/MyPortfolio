20047a0bacf077dd917cc2a8293738bede775cb9
Kundan <100273989+InfiniteCoder100@users.noreply.github.com>
2023-12-30 21:40:27 +0530

    final commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Build and Deploy an Amazing 3D Developer Portfolio in React with Three.js
+
+Author : Kundan Kumar
---
+++ src/constants/index.js
@@ -0,0 +1,216 @@
+import { meta, shopify, starbucks, tesla } from "../assets/images";
+import {
+  car,
+  contact,
+  css,
+  estate,
+  express,
+  git,
+  github,
+  html,
+  javascript,
+  linkedin,
+  mongodb,
+  motion,
+  mui,
+  nextjs,
+  nodejs,
+  pricewise,
+  react,
+  redux,
+  sass,
+  data,
+  snapgram,
+  summiz,
+  tailwindcss,
+  threads,
+  typescript,
+} from "../assets/icons";
+
+export const skills = [
+  {
+    imageUrl: css,
+    name: "CSS",
+    type: "Frontend",
+  },
+  {
+    imageUrl: express,
+    name: "Express",
+    type: "Backend",
+  },
+  {
+    imageUrl: git,
+    name: "Git",
+    type: "Version Control",
+  },
+  {
+    imageUrl: github,
+    name: "GitHub",
+    type: "Version Control",
+  },
+  {
+    imageUrl: html,
+    name: "HTML",
+    type: "Frontend",
+  },
+  {
+    imageUrl: javascript,
+    name: "JavaScript",
+    type: "Frontend",
+  },
+  {
+    imageUrl: mongodb,
+    name: "MongoDB",
+    type: "Database",
+  },
+  {
+    imageUrl: motion,
+    name: "Motion",
+    type: "Animation",
+  },
+  {
+    imageUrl: mui,
+    name: "Material-UI",
+    type: "Frontend",
+  },
+  {
+    imageUrl: nextjs,
+    name: "Next.js",
+    type: "Frontend",
+  },
+  {
+    imageUrl: nodejs,
+    name: "Node.js",
+    type: "Backend",
+  },
+  {
+    imageUrl: react,
+    name: "React",
+    type: "Frontend",
+  },
+  {
+    imageUrl: redux,
+    name: "Redux",
+    type: "State Management",
+  },
+  {
+    imageUrl: sass,
+    name: "Sass",
+    type: "Frontend",
+  },
+  {
+    imageUrl: tailwindcss,
+    name: "Tailwind CSS",
+    type: "Frontend",
+  },
+  {
+    imageUrl: typescript,
+    name: "TypeScript",
+    type: "Frontend",
+  },
+];
+
+export const experiences = [
+  {
+    title: "Blockchain Developer",
+    company_name: "Metacrafters",
+    icon: starbucks,
+    iconBg: "#accbe1",
+    date: "July 2023 - Dec 2023",
+    points: [
+      "Developing and maintaining smart contracts and testing it .",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "React Developer",
+    company_name: "Otakukart",
+    icon: tesla,
+    iconBg: "#fbc3bc",
+    date: "Jan 2023 - May 2023",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "Web Developer",
+    company_name: "Fiverr",
+    icon: shopify,
+    iconBg: "#b7e4c7",
+    date: "Jan 2022 - Jan 2023",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+];
+
+export const socialLinks = [
+  {
+    name: "Contact",
+    iconUrl: contact,
+    link: "/contact",
+  },
+  {
+    name: "GitHub",
+    iconUrl: github,
+    link: "https://github.com/InfiniteCoder100",
+  },
+  {
+    name: "LinkedIn",
+    iconUrl: linkedin,
+    link: "https://www.linkedin.com/in/kundandevner101/",
+  },
+];
+
+export const projects = [
+  {
+    iconUrl: pricewise,
+    theme: "btn-back-red",
+    name: "LSD",
+    description:
+      "LSD is a liquid staking and leverage staking based yield generating vault. You can deposit your matic and receive LSD tokens which will appreciate in value w.r.t. matic over time 📈 LSD internally uses Stader, Aave & Balancer as well as the whole project is build using scaffold-eth 2 🏗️",
+    link: "https://github.com/InfiniteCoder100/lsd",
+  },
+
+  {
+    iconUrl: threads,
+    theme: "btn-back-green",
+    name: "CherrySwap",
+    description:
+      "CherrySwap is a new NFT Marketplace in the town build for buying , selling and swapping of NFTS OR any Token.",
+    link: "https://github.com/InfiniteCoder100/CherrySwap",
+  },
+  {
+    iconUrl: data,
+    theme: "btn-back-blue",
+    name: "DataX",
+    description:
+      "DataX is a bid/ask marketplace concept for data similar to StockX where users can list data their unique and custom datasets for sale with the ability for public search and potential buyers to set their asking price.",
+    link: "https://github.com/InfiniteCoder100/DataX",
+  },
+
+  {
+    iconUrl: estate,
+    theme: "btn-back-black",
+    name: "CreepPay",
+    description:
+      "CreepPay is a Web3 Payment Interface Application for exchanging money across borders with very less effort and time, removing the long time delay and complexities in FACED traditional payment gateways.",
+    link: "https://github.com/InfiniteCoder100/CreepPay_Dapp",
+  },
+  {
+    iconUrl: summiz,
+    theme: "btn-back-yellow",
+    name: "SAAVE",
+    description:
+      "Welcome SAAVE finance, designed to provide an easy and simple way for beginners to enter the world of decentralized finance. Our protocol offers a user-friendly interface that allows you to track your profits in real-time, making it easy to manage your investments.",
+    link: "https://github.com/InfiniteCoder100/SAAVE",
+  },
+];
