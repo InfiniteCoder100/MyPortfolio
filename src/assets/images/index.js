ffe731c87f9054bce94e2421df7a1bc5f36bbca1
Kundan <100273989+InfiniteCoder100@users.noreply.github.com>
2023-12-30 23:02:38 +0530

    all fc

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,15 +1,21 @@
-import hero from './hero.jpg'
-import meta from './meta.png'
-import shopify from './shopify.png'
-import starbucks from './starbucks.png'
-import tesla from './tesla.png'
-import logo from './logo.svg'
+import hero from "./hero.jpg";
+import meta from "./meta.png";
+import metacrafters from "./metacrafters.jpeg";
+import fiverr from "./fiverr.png";
+import otaku from "./otaku.jpeg";
+import shopify from "./shopify.png";
+import starbucks from "./starbucks.png";
+import tesla from "./tesla.png";
+import logo from "./logo.jpg";
 
 export {
-    hero,
-    meta,
-    shopify,
-    starbucks,
-    tesla,
-    logo
-}
+  hero,
+  meta,
+  shopify,
+  starbucks,
+  tesla,
+  logo,
+  otaku,
+  metacrafters,
+  fiverr,
+};
